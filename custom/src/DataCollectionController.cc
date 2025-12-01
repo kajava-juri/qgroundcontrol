803f916b02aeebbf0ffbaf9c102e10af3f414bfc
Jüri Kajava <73351309+kajava-juri@users.noreply.github.com>
2025-12-01 11:08:36 +0200

    Implement JSON body in HTTP requests
    Added JSON support to HTTP requests in DataCollectionController.

--- custom/src/DataCollectionController.cc
+++ custom/src/DataCollectionController.cc
@@ -3,6 +3,8 @@
 #include "QGCLoggingCategory.h"
 #include <QtNetwork/QNetworkReply>
 #include <QtNetwork/QNetworkAccessManager>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QJsonObject>
 
 QGC_LOGGING_CATEGORY(DataCollectionControllerLog, "Custom.DataCollectionController")
 
@@ -30,14 +32,29 @@ void DataCollectionController::sendHttpRequest(QString endpoint) {
     qCDebug(DataCollectionControllerLog) << "Sending HTTP request to endpoint:" << endpoint;
     
     QNetworkRequest request(QUrl(QString("http://127.0.0.1:5000/" + endpoint)));
-    QNetworkReply* reply = _networkManager.post(request, QByteArray());
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
+    
+    // Create JSON body (empty object, Flask will use defaults)
+    QJsonObject jsonObj;
+    QJsonDocument jsonDoc(jsonObj);
+    QByteArray jsonData = jsonDoc.toJson();
+    
+    QNetworkReply* reply = _networkManager.post(request, jsonData);
 
-    connect(reply, &QNetworkReply::finished, this, [reply, endpoint]() {
+    connect(reply, &QNetworkReply::finished, this, [this, reply, endpoint]() {
         if (reply->error() == QNetworkReply::NoError) {
             QByteArray responseData = reply->readAll();
             qCDebug(DataCollectionControllerLog) << "HTTP request to" << endpoint << "succeeded. Response:" << responseData;
         } else {
             qCWarning(DataCollectionControllerLog) << "HTTP request to" << endpoint << "failed. Error:" << reply->errorString();
+            // Revert the isCollecting state on error
+            if (endpoint == "start") {
+                _isCollecting = false;
+                emit isCollectingChanged();
+            } else if (endpoint == "stop") {
+                _isCollecting = true;
+                emit isCollectingChanged();
+            }
         }
         reply->deleteLater();
     });
